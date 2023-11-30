3828b4fbde4027dc0ae8bab0eedee8b87ce001b4
ASaintG <78000780+ASaintG@users.noreply.github.com>
2023-11-30 16:33:07 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//1.Crea una variable llamada edad y asígnale tu edad.
+let edad=25;
+//2.Declara una constante nombre y asígnala a tu nombre.
+const nombre="Anthony Saint Hernandez Gonzalez";
+//3.Crea una variable estudiante y asígnala a un valor booleano que indique si eres estudiante.
+let estudiante= true;
+//4.Escribe una función que tome un número y devuelva su doble. Prueba esta función con tu edad.
+function numeros(numero){
+    return(numero*2)
+}
+//5.Imprime en consola un mensaje que incluya tu nombre, edad, si eres estudiante y el doble de tu edad.
+console.log(nombre,edad,estudiante,numeros(edad));
